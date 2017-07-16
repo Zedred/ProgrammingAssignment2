5bb3b85d39013c3cc27a34e44991cb327f693399
Charles Wyman <chevwy@umich.edu>
2017-07-16 17:03:15 -0400

    Coursera data science week 3 assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The following functions together operate on square matrices
+## in order to capture and invert it. 
 
-## Write a short comment describing this function
+## makeCacheMatrix() operates by calling the function as an
+## assignment, for example:
+##    test_matrix$set(): sets the values of test_matrix
+##    test_matrix$get(): returns the value of the matrix
+##    test_matrix$setinverse(): calculates the inverse of 
+##      test_matrix
+##    test_matrix$getinverse(): returns the previously
+##      calculated inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    if (sqrt(length(y))%%1==0){
+      x <<- matrix(y, nrow =sqrt(length(y)), ncol = sqrt(length(y)))
+      m <<- NULL
+    } else
+      print("Matrix would not be square, please enter a list of length 1, 4, 9, 16, etc.")
+  }
+  get <- function() x
+  setinverse <- function(solve) m <<- solve
+  getinverse <- function() m
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve() calculates the value of the provided matrix,
+##  this matrix must be created using the makeCacheMatrix()
+##  function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getinverse()
+  if(!is.null(m)){
+    message("Getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinverse(m)
+  m
 }
